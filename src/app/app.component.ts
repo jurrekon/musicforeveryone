8cde586e2f6e20312bf9b8acd8688a76eac6898f
Jurre Kon <99034766@mydavinci.nl>
2017-11-27 11:22:19 +0100

    stuff

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpHeaders} from '@angular/common/http';
-import {ChatComponent} from "./chat/chat.component";
+import { ChatComponent } from "./chat/chat.component";
 
 @Component({
   selector: 'app-root',
@@ -13,13 +13,13 @@ export class AppComponent {
   constructor(private http: HttpClient){}
 
   public login(){
-    this.http.get('http://localhost:8888/test').subscribe((res:Response) => {
+    this.http.get('http://localhost:5000/test').subscribe((res:Response) => {
       console.log(res);
     });
   }
 
   public chat(){
-    this.http.get('http://localhost:8888/chat').subscribe((res:Response) => {
+    this.http.get('http://localhost:5000/chat').subscribe((res:Response) => {
       console.log(res);
     });
   }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,9 @@ import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { ChatComponent } from './chat/chat.component';
-import {CUSTOM_ELEMENTS_SCHEMA} from "@angular/core";
+import { CUSTOM_ELEMENTS_SCHEMA } from "@angular/core";
 import { FormsModule } from '@angular/forms';
-import {searchPageComponent} from './searchPage';
+import { searchPageComponent } from './searchPage';
 import { routes } from './app.router';
 
 @NgModule({
