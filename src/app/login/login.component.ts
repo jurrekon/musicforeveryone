98e6eeca29b0d70ec68463b8070549d19e067953
Jurre Kon <99034766@mydavinci.nl>
2017-12-19 10:08:34 +0100

    commit

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component } from '@angular/core';
 
 import { Http,Headers } from '@angular/http';
+import { AppComponent } from '../app.component';
 
 @Component({
     styleUrls:['login.component.css'],
@@ -11,6 +12,7 @@ import { Http,Headers } from '@angular/http';
 
 export class LoginComponent{
     user;
+    firstname = '';
 
     constructor(private http: Http){}
     ngOnInit(){}
@@ -21,6 +23,12 @@ export class LoginComponent{
         let headers = new Headers({ 'Content-Type': 'application/json' });
         return this.http.post('http://localhost:4100/login', JSON.stringify(this.user), {headers: headers}).subscribe(data => {
             console.log(data);
+            var body = JSON.parse(data['_body']);
+            if(body['status'] == 'succes'){
+                // set all the variables
+            } else {
+                // error
+            }
         });
     }
 }
